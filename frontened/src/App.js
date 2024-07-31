a6dc878e9f36b3990016ec7521f2db0aa9778470
Anshika Agarwal <anshikaagarwal0405@gmail.com>
2024-07-31 15:57:27 +0530

    full project

---
+++ frontened/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import AddWaste from './components/AddWaste';
+
+function App() {
+  return (
+  <>
+  <AddWaste/></>
+  );
+}
+
+export default App;
---
+++ frontened/src/components/AddWaste.js
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from 'react';
+import './AddWaste.css'
+function AddWaste() {
+    const [wastes, setWastes] = useState([]);
+
+    useEffect(() => {
+        getWastes();
+    }, []);
+
+    const getWastes = async () => {
+        try {
+            let result = await fetch("http://localhost:8000/getAll", {
+                method: "get",
+            });
+            result = await result.json();
+            setWastes(result);
+        } catch (error) {
+            console.log("Error in fetching wastes");
+        }
+    };
+
+    const deleteWaste = async (id) => {
+        try {
+            let result = await fetch(`http://localhost:8000/delete/${id}`, {
+                method: "DELETE",
+            });
+            result = await result.json();
+            if (result) {
+                getWastes();
+            }
+        } catch (error) {
+            console.log('Error in deleting the waste from the list');
+        }
+    };
+    const searchHandle = async (event) => {
+        let key = event.target.value;
+        if (key) {
+            try {
+                let result = await fetch(`http://localhost:8000/search/${key}`);
+                result = await result.json();
+                setWastes(result);
+            } catch (error) {
+                console.error('Error searching products:', error);
+            }
+        } else {
+            getWastes();
+        }
+    };
+
+    return (
+        <div className='waste-list'>
+            <h1>Waste List</h1>
+            <input className="search" type="text" placeholder='Search Product' onChange={searchHandle} />
+            <ul className='waste'>
+                <li className='waste'>S.No</li>
+                <li>Name</li>
+                <li>Type</li>
+                <li>Description</li>
+                <li>Complain</li>
+                <li>Operations</li>
+            </ul>
+            
+           
+            {
+                wastes.length > 0 ? wastes.map((item, index) => (
+                    <ul key={item._id}>
+                        <li>{index + 1}</li>
+                        <li>{item.Name}</li>
+                        <li>{item.Type}</li>
+                        <li>{item.Desc}</li>
+                        <li>{item.Comp}</li>
+                        <li>
+                            <button onClick={() => deleteWaste(item._id)}>Delete</button>
+                        </li>
+                    </ul>
+                )) : <p>No waste items found.</p>
+            }
+        </div>
+    );
+}
+
+export default AddWaste;
